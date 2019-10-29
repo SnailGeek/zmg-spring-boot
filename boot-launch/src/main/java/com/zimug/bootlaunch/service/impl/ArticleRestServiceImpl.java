d4849c7377f3942daf43871de0bcf0b4182021d0
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2019-10-29 15:52:15 +0800

    上次提交备注错误，应该是Spring JDBC JTA实现分布式事务

--- boot-launch/src/main/java/com/zimug/bootlaunch/service/impl/ArticleRestServiceImpl.java
+++ boot-launch/src/main/java/com/zimug/bootlaunch/service/impl/ArticleRestServiceImpl.java
@@ -32,10 +32,10 @@ public class ArticleRestServiceImpl implements ArticleRestService {
     @Resource
     private JdbcTemplate secondaryJdbcTemplate;
 
-    @Transactional
+    @Transactional //如果只用这个注解一旦出错，还是有数据库里的数据会被修改，此例中secondaryJdbcTemplate中的数据被修改，因为是面向切面编程，先打开的后关闭
     public Article saveArticle(Article article) {
-        articleJDBCDAO.save(article, secondaryJdbcTemplate);
         articleJDBCDAO.save(article, primaryJdbcTemplate);
+        articleJDBCDAO.save(article, secondaryJdbcTemplate);
 //        int a = 2/0;
         return article;
     }
